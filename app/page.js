f588a9a68e830336087cdeba803cebfba23b867e
georgeonweb3 <139614108+georgeonweb3@users.noreply.github.com>
2026-07-03 22:01:18 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
+import BasePulse from "./BasePulse";
+
 export default function Home() {
-  return (
-    <div style={{ background: "blue", color: "white", padding: 40, fontSize: 32 }}>
-      BasePulse is alive
-    </div>
-  );
-}
+  return <BasePulse />;
+    }
