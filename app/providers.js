ad044348efb21c34271eaac4b80b53136f335bca
georgeonweb3 <139614108+georgeonweb3@users.noreply.github.com>
2026-07-03 21:32:57 +0100

    Create providers.js

---
+++ app/providers.js
@@ -0,0 +1,25 @@
+"use client";
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { base } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+
+const config = getDefaultConfig({
+  appName: "BasePulse",
+  projectId: "YOUR_PROJECT_ID_HERE",
+  chains: [base],
+  transports: { [base.id]: http("https://mainnet.base.org") },
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
